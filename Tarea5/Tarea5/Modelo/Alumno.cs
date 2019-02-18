7b1d217b2288b598ce5165111cc11a4a668d9120
Alejandro Brito <47316052+Brito9539@users.noreply.github.com>
2019-02-18 02:21:20 -0600

    Tarea 5-Commit

---
+++ Tarea5/Tarea5/Modelo/Alumno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea5.Modelo
+{
+    class Alumno
+    {
+        int Matricula;
+        string Nombre;
+        string ApMaterno;
+        string ApPaterno;
+
+        public int Matricula1 { get => Matricula; set => Matricula = value; }
+        public string Nombre1 { get => Nombre; set => Nombre = value; }
+        public string ApMaterno1 { get => ApMaterno; set => ApMaterno = value; }
+        public string ApPaterno1 { get => ApPaterno; set => ApPaterno = value; }
+    }
+}
---
+++ Tarea5/Tarea5/ModeloVista/QuerysMysql.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Tarea5.ModeloVista
+{
+    class QuerysMysql
+    {
+        MySqlCommand cmd = new MySqlCommand();
+        MySqlConnection cn;
+
+        public void Editar(string Nombre, string ApMaterno, string ApPaterno, int Matricula)
+        {
+            cn = Conexion.conectar();
+            cmd.Connection = cn;
+
+
+            cmd.CommandText = "UPDATE alumno SET Nombre = '" + Nombre + "',Ap_Materno ='" + ApMaterno + "',Ap_Paterno = '" + ApPaterno + "'  WHERE Matricula = '" + Matricula + "'";
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public void Agregar(string Nombre, string ApMaterno, string ApPaterno, int Matricula)
+        {
+            cn = Conexion.conectar();
+            cmd.Connection = cn;
+            cmd.CommandText = "INSERT INTO alumno(Matricula, Nombre, Ap_Materno, Ap_Paterno) VALUES('" + Matricula + "','" + Nombre + "','" + ApMaterno + "','" + ApPaterno + "')";
+            cmd.ExecuteNonQuery();
+        }
+
+        public void Eliminar(int Matricula)
+        {
+            cn = Conexion.conectar();
+            cmd.Connection = cn;
+            cmd.CommandText = "DELETE FROM alumno WHERE Matricula='" + Matricula + "'";
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
